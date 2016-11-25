b9f7fc90796c665c1ce16711ace54abe96875e9f
David Parlevliet <dave@gankzone.com>
2016-11-25 12:14:03 +1100

    added install file

---
+++ install/ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes build-essential python2.7
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes nodejs supervisor git
+cd /root
+git clone https://github.com/dparlevliet/fer.git
+npm install
