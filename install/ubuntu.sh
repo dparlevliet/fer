8bfb7d232fa8d8f1e43c0d51e3009541011e8a41
David Parlevliet <dave@gankzone.com>
2016-11-27 15:18:07 +1100

    fixed installer

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -5,4 +5,5 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes nodejs supervisor git
 cd /root
 git clone https://github.com/dparlevliet/fer.git
+cd /root/fer
 npm install
